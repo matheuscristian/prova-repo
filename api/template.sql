7c0e79e012a78b2d7bb88a17f5009371c5985b51
Matheus Cristian <matheuscristianrg@gmail.com>
2025-06-12 19:48:27 -0300

    Tinha mandado o arquivo SQL errado :(

--- api/template.sql
+++ api/template.sql
@@ -7,11 +7,10 @@ CREATE TABLE bibliotecario (
 CREATE TABLE livro (
     id SERIAL PRIMARY KEY NOT NULL,
     bibliotecario SERIAL NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
     titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
     genero VARCHAR(255) NOT NULL,
-    status VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL DEFAULT 'Disponível',
     created_date DATE NOT NULL,
     Foreign Key (bibliotecario) REFERENCES bibliotecario (id)
 );
